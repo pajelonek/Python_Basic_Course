344470c591acbe2ebe3df0758422ef02df23fc15
pajelonek <pawel.jelonek.krakow@gmail.com>
2019-10-22 16:02:27 +0200

    Fixing solve1.py solution.

--- lekcja_8/solve1.py
+++ lekcja_8/solve1.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
 
+
 def solve1(a, b, c):
     """Rozwiązywanie równania liniowego a x + b y + c = 0."""
     if a == 0 and b != 0:
-        return (-c) / 1
+        y = (-c) / b
+        return "Rozwiazaniem jest liczba " + str(y)
     if b == 0:
-        return (-c) / a
+        y = (-c) / a
+        return "Rozwiazaniem jest liczba " + str(y)
+    return "Rozwiazaniem jest prosta spelniajaca rownanie: y = (" + str(-a) + "x" + str(-c) + ") / " + str(b)
 
-    # def f(x):
-    #     return (a * x + c) / b
-    #
-    # argx = range(0, 100)
-    # argy = []
-    # for i in range(0, 100):
-    #     argy.append(f(i))
+
+def main():
+    print("Wynik dla 10:" + str(solve1(2, 3, 4)))
+
+
+if __name__ == '__main__':
+    main()  # uruchamia wszystkie testy
