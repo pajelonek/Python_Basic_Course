c7593c37d191d8f5b080a02b8e2f0f4b9ff99320
pajelonek <pawel.jelonek.krakow@gmail.com>
2019-12-22 16:40:02 +0100

    Adding few more basic tests for show and is_empty methods

--- projekt/TestFibHeap.py
+++ projekt/TestFibHeap.py
@@ -4,6 +4,15 @@ from FibHeap import FibHeap
 
 class TestQueue(unittest.TestCase):
 
+    def test_error_show(self):
+        heap_test_show = FibHeap()
+        self.assertEqual(heap_test_show.show_main_nodes(), "")
+        heap_test_show.push(5)
+        heap_test_show.push(2)
+        heap_test_show.push(9)
+        heap_test_show.push(11)
+        self.assertEqual(heap_test_show.show_main_nodes(), "5 2 9 11 ")
+
     def test_error_push(self):
         heap = FibHeap()
         self.assertEqual(heap.show_main_nodes(), "")
@@ -19,11 +28,11 @@ class TestQueue(unittest.TestCase):
     def test_error_pop(self):
         pass
 
-    def test_error_is_full(self):
-        pass
-
     def test_error_is_empty(self):
-        pass
+        heap = FibHeap()
+        self.assertEqual(heap.is_empty(), True)
+        heap.push(5)
+        self.assertEqual(heap.is_empty(), False)
 
 
 if __name__ == '__main__':
