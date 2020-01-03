ba62fcee9482c6741c0ab8e15d57022ac6b5f2ab
pajelonek <pawel.jelonek.krakow@gmail.com>
2020-01-03 13:05:57 +0100

    Adding correct tests for randomqueue

--- lekcja_10/TestRandomQueue.py
+++ lekcja_10/TestRandomQueue.py
@@ -5,17 +5,21 @@ from RandomQueue import RandomQueue
 class TestRandomQueue(unittest.TestCase):
 
     def test_insert_push(self):
-        queue = RandomQueue(size=3)
+        queue = RandomQueue()
         queue.insert(1)
         queue.insert(2)
         queue.insert(3)
+        queue.insert(4)
+        queue.insert(5)
         self.assertEqual(queue.is_full(), True)
-        self.assertEqual(queue.items[1], 1)
-        self.assertEqual(queue.items[2], 2)
-        self.assertEqual(queue.items[3], 3)
+        self.assertEqual(queue.items[0], 1)
+        self.assertEqual(queue.items[1], 2)
+        self.assertEqual(queue.items[2], 3)
+        self.assertEqual(queue.items[3], 4)
+        self.assertEqual(queue.items[4], 5)
 
     def test_remove_pop(self):
-        queue = RandomQueue(size=5)
+        queue = RandomQueue()
         queue.insert(4)
         queue.insert(3)
         queue.insert(7)
@@ -23,18 +27,20 @@ class TestRandomQueue(unittest.TestCase):
         self.assertEqual(queue.is_empty(), False)
         queue.remove()
         queue.remove()
-        self.assertEqual(queue.is_empty(), True)
+        self.assertEqual(queue.is_empty(), False)
 
     def test_error_is_full(self):
-        queue = RandomQueue(size=3)
+        queue = RandomQueue()
         queue.insert(4)
         queue.insert(3)
         queue.insert(7)
+        queue.insert(8)
+        queue.insert(1)
         with self.assertRaises(ValueError):
             queue.insert(4)
 
     def test_error_is_empty(self):
-        queue = RandomQueue(size=3)
+        queue = RandomQueue()
         with self.assertRaises(ValueError):
             queue.remove()
 
